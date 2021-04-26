03e63fdf1ad4895743923b9daab629c25db4b5cb
subramaniannk <subramanian.nk@live.com>
2021-04-26 12:04:17 +0530

    including the orbit class

---
+++ orbit.py
@@ -0,0 +1,7 @@
+class Orbit:
+    def __init__(self, road, no_of_craters, orbit_traffic_speed):
+        self.road = road
+        self.no_of_craters = no_of_craters
+        self.orbit_traffic_speed= orbit_traffic_speed
+
+
