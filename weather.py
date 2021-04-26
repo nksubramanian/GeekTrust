b99b3bee9f9fa370a70ec938b59e7ecbdf60b0d5
subramaniannk <subramanian.nk@live.com>
2021-04-26 12:11:55 +0530

    including the weather

---
+++ weather.py
@@ -0,0 +1,6 @@
+class Weather:
+    def __init__(self, name, crater_change, ):
+        self.name = name
+        self.crater_change = crater_change
+        self.vehicle_type_allowed = vehicle_type_allowed
+        
