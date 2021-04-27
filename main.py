625f7a64fcff18863c5b428c67bb443d64962161
subramaniannk <subramanian.nk@live.com>
2021-04-27 11:24:10 +0530

    orbit refactoring done

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from vehicle import VehicleFactory
 from decisionmaker import DecisionMaker
 
 if __name__ == '__main__':
-    ORBIT1 = Orbit("ORBIT1", 18, 20, 89)
-    ORBIT2 = Orbit("ORBIT2", 20, 10, 95)
+    orbit1 = Orbit("ORBIT1", 18, 20)
+    orbit2 = Orbit("ORBIT2", 20, 10)
+    orbit1.set_orbit_traffic_speed(89)
+    orbit2.set_orbit_traffic_speed(96)
+    print(orbit1.orbit_traffic_speed)
+    print(orbit2.orbit_traffic_speed)
     user_weather = WeatherFactory.create_weather("Windy")
     vehicles_allowed = VehicleFactory.get_vehicles(user_weather.allowed_vehicles)
-    orbits = [ORBIT1, ORBIT2]
+    orbits = [orbit1, orbit2]
     weather_adjusted_orbits = user_weather.adjust_orbits(orbits)
     decision = DecisionMaker().find_quickest_orbit_and_vehicle(vehicles_allowed, weather_adjusted_orbits)
     print(decision[0].vehicle_type)
--- orbit.py
+++ orbit.py
@@ -1,7 +1,10 @@
 class Orbit:
-    def __init__(self, name, distance, no_of_craters, orbit_traffic_speed):
+    def __init__(self, name, distance, no_of_craters):
         self.name = name
         self.distance = distance
         self.no_of_craters = no_of_craters
+        self.orbit_traffic_speed = None
+
+    def set_orbit_traffic_speed(self, orbit_traffic_speed):
         self.orbit_traffic_speed = orbit_traffic_speed
 
