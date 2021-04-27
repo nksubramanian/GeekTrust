700d23d9f99823c6c23c2de6564953ed6eeff274
subramaniannk <subramanian.nk@live.com>
2021-04-27 20:06:46 +0530

    r

--- assistance.py
+++ assistance.py
@@ -11,11 +11,11 @@ class Assistance:
         orbitRepo = OrbitRepository()
         orbits = orbitRepo.get_orbits()
 
-        self.orbit1 = orbits[0]
-        self.orbit2 = orbits[1]
+        orbit1 = orbits[0]
+        orbit2 = orbits[1]
 
-        self.orbit1.set_orbit_traffic_speed(traffic_speed_limits[0])
-        self.orbit2.set_orbit_traffic_speed(traffic_speed_limits[1])
+        orbit1.set_orbit_traffic_speed(traffic_speed_limits[0])
+        orbit2.set_orbit_traffic_speed(traffic_speed_limits[1])
 
         self.user_weather = WeatherFactory.create_weather(weather)
 
@@ -24,7 +24,7 @@ class Assistance:
 
 
         allowed_vehicles = VehicleCreator.create_vehicles(self.user_weather.allowed_vehicles)
-        orbits = [self.orbit1, self.orbit2]
+        orbits = [orbit1, orbit2]
         weather_adjusted_orbits = self.user_weather.adjust_orbits(orbits)
         travel_options = list(map(TravelOption, orbits * len(allowed_vehicles),
                                   allowed_vehicles * len(weather_adjusted_orbits)))
