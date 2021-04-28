f755d590a095359e4581c7e882fafac1301ae5c1
subramaniannk <subramanian.nk@live.com>
2021-04-28 18:02:05 +0530

    alignment

--- orbit.py
+++ orbit.py
@@ -10,14 +10,13 @@ class Orbit:
 
 
 class OrbitRepository:
-    # read this from a config file
     def get_orbits(self):
         from configparser import ConfigParser
         config = ConfigParser()
         config.read('F:\\GeekTrust\\config.ini')
         orbit1_name = config["ORBIT1"]["name"]
         orbit1_distance = int(config["ORBIT1"]["distance"])
-        orbit1_no_of_craters= int(config["ORBIT1"]["no_of_craters"])
+        orbit1_no_of_craters = int(config["ORBIT1"]["no_of_craters"])
         orbit2_name = config["ORBIT2"]["name"]
         orbit2_distance = int(config["ORBIT2"]["distance"])
         orbit2_no_of_craters = int(config["ORBIT2"]["no_of_craters"])
