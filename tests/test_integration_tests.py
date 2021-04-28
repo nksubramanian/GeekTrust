f0c4d8f51b83226b5b113b358cf01dbe218fd9c6
subramaniannk <subramanian.nk@live.com>
2021-04-28 17:45:44 +0530

    Couple of integration test done

--- tests/test_integration_tests.py
+++ tests/test_integration_tests.py
@@ -5,6 +5,15 @@ from weather import VehicleType
 
 class VehicleTest(unittest.TestCase):
     def test_when_vehicle_is_bike_then_create_vehicle_returns_bike(self):
-        travel_option = TravelOptionProvider().get_best_travel_option("RAINY", [40, 25])
-        assert travel_option.vehicle_type == VehicleType.CAR
-        assert travel_option.orbit_name == "ORBIT2"
+        tests = [["RAINY", [40, 25], (VehicleType.CAR, "ORBIT2")],
+                 ["RAINY", [12, 12], (VehicleType.TUKTUK, "ORBIT2")],
+                 ["SUNNY", [50, 50], (VehicleType.CAR, "ORBIT2")],
+                 ["SUNNY", [5, 50],  (VehicleType.CAR, "ORBIT2")],
+                 ["SUNNY", [5, 50], (VehicleType.CAR, "ORBIT2")]]
+
+        for test in tests:
+            travel_option = TravelOptionProvider().get_best_travel_option(test[0], test[1])
+            assert (travel_option.vehicle_type, travel_option.orbit_name ) == test[2]
+
+
+
