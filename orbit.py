fdae5420cd4c9132c90cdc1a669fb7c22533aa7f
subramaniannk <subramanian.nk@live.com>
2021-04-29 20:29:16 +0530

    Refactored tests

--- orbit.py
+++ orbit.py
@@ -1,15 +1,23 @@
 class Orbit:
     def __init__(self, name, distance, no_of_craters):
         self.__name = name
-        self.distance = distance
+        self.__distance = distance
         self.no_of_craters = no_of_craters
         self.orbit_traffic_speed = None
+
+    def get_distance(self):
+        return self.__distance
 
     def set_orbit_traffic_speed(self, orbit_traffic_speed):
         self.orbit_traffic_speed = orbit_traffic_speed
 
     def get_orbit_name(self):
         return self.__name
+
+    #get crater
+    #get distance
+    #get_traffic_speed_limit
+
 
 
 class OrbitRepository:
@@ -21,4 +29,5 @@ class OrbitRepository:
         for orbit_parameters in self.orbits_parameters:
             orbit_repository.append(Orbit(orbit_parameters[0], orbit_parameters[1], orbit_parameters[2]))
         return orbit_repository
+
 
--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -4,29 +4,32 @@ from orbit import OrbitRepository, Orbit
 from vehicle import VehicleCreator, Vehicle
 from weather import Weather, WeatherFactory, VehicleType
 from travel_option import TravelOption
+from travel_option_provider import TravelOptionProvider
+
 
 class OrbitTest(unittest.TestCase):
 
-    def test_get_orbits_return_orbits_with_specifications_as_mentioned_in_question(self):
+    def test_orbit_creation_test(self):
+        orb = Orbit("ORBIT1", 18, 20)
+        assert orb.get_orbit_name() == "ORBIT1"
+        assert orb.distance == 18
+        assert orb.no_of_craters == 20
+        assert orb.orbit_traffic_speed is None
+
+    def test_traffic_speed_could_be_set_for_orbit(self):
+        test_orbit = Orbit("ORBIT1", 18, 20)
+        test_orbit.set_orbit_traffic_speed(54)
+        assert test_orbit.orbit_traffic_speed == 54
+
+
+class OrbitRepositoryTest(unittest.TestCase):
+    def test_orbit_repository_produces_orbit_as_per_config(self):
         orbit_parameters = [["ORBIT1", 18, 20], ["ORBIT2", 20, 10]]
         orbit_repository = OrbitRepository(orbit_parameters)
         orbits = orbit_repository.get_orbits()
-        values = [[0, ("ORBIT1", 18, 20)], [1, ("ORBIT2", 20, 10)]]
-        for value in values:
-            assert (orbits[value[0]].get_orbit_name(), orbits[value[0]].distance, orbits[value[0]].no_of_craters) == value[1]
-
-    def test_traffic_speed_limit_gets_added_correctly(self):
-        orbit_parameters = [["ORBIT1", 18, 20], ["ORBIT2", 20, 10]]
-        orbit_repository = OrbitRepository(orbit_parameters)
-        orbits = orbit_repository.get_orbits()
-        assert orbits[0].orbit_traffic_speed is None
-        assert orbits[1].orbit_traffic_speed is None
-        orbits[0].set_orbit_traffic_speed(45)
-        orbits[1].set_orbit_traffic_speed(54)
-        assert orbits[0].orbit_traffic_speed == 45
-        assert orbits[1].orbit_traffic_speed == 54
-
-
+        values = [("ORBIT1", 18, 20), ("ORBIT2", 20, 10)]
+        for i in range(0, len(values)):
+            assert (orbits[i].get_orbit_name(), orbits[i].distance, orbits[i].no_of_craters) == values[i]
 
 
 class VehicleTest(unittest.TestCase):
@@ -36,7 +39,7 @@ class VehicleTest(unittest.TestCase):
         assert vehicle.get_vehicle_type() == VehicleType.CAR
 
 
-    def test_minimum_of_orbit_traffic_speed_and_vehicle_speed_is_used_to_compute_time_for_orbits(self):
+    def test_vehicle_takes_minimum_of_orbit_traffic_speed_and_vehicle_speed_is_used_to_compute_time_for_orbits(self):
         vehicle = Vehicle(VehicleType.CAR, 10, 60)
         orbit = Orbit("ORBIT1", 20, 10)
         orbit.set_orbit_traffic_speed(45)
@@ -47,12 +50,12 @@ class VehicleTest(unittest.TestCase):
         assert vehicle.compute_time_for_orbit(orbit) == 14
 
     class WeatherTest(unittest.TestCase):
-        def test_weather_can_be_created_as_specified_attributed(self):
-            weather = Weather(10, [VehicleType.CAR, VehicleType.BIKE])
+        def test_weather_can_be_created_with_attributes_as_required(self):
+            weather = Weather(10, [VehicleType.TUKTUK, VehicleType.BIKE])
+            assert weather.get_crater_change_percentage() == 10
+            assert weather.get_allowed_vehicles() == [VehicleType.TUKTUK, VehicleType.BIKE]
 
-
-
-        def test_weather_is_created_correctly_based_on_weather_string(self):
+        def test_weather_attribtues_is_created_correct_based_on_weather_string(self):
             tests = [["SUNNY", -10, [VehicleType.CAR, VehicleType.BIKE, VehicleType.TUKTUK]],
                      ["WINDY", 0, [VehicleType.CAR, VehicleType.BIKE]],
                      ["RAINY", 20, [VehicleType.CAR, VehicleType.TUKTUK]]]
@@ -62,25 +65,28 @@ class VehicleTest(unittest.TestCase):
                 assert weather.get_crater_change_percentage() == test[1]
 
 
-        def test_craters_of_orbit_are_modified_correctly_based_on_weather(self):
+        def test_weather_modifies_no_of_craters_in_orbits(self):
             orbit_test = Orbit("ORBIT1", 20, 10)
-            weather = Weather(-10, [VehicleType.CAR, VehicleType.TUKTUK])
+            weather = Weather(-10, [VehicleType.BIKE, VehicleType.TUKTUK])
             assert orbit_test.no_of_craters == 10
             weather.adjust_crater(orbit_test)
             assert orbit_test.no_of_craters == 9
 
 
+    class PreferenceTest(unittest.TestCase):
 
+        def test_preference_of_bike_tuktuk_car_is_used(self):
+            orbits = [Orbit("ORBIT1", 18, 20), Orbit("ORBIT2", 20, 20)]
+            vehicles = [Vehicle(VehicleType.CAR, 10, 60), Vehicle(VehicleType.BIKE, 10, 60), Vehicle(VehicleType.TUKTUK, 10, 60)]
+            travel_options = TravelOptionProvider.get_travel_options(orbits, vehicles)
+            print(travel_options)
+            travel_options = TravelOptionProvider.get_fastest_travel_options(travel_options)
+            x = TravelOptionProvider.get_best_travel_option_based_on_vehicle(travel_options)
 
+            print(x.get_vehicle_type())
+            assert True
 
-
-
-
-
-
-
-
-
-
-
+        def test_preference_of_bike_tuktuk_car_is_useddsac(self):
+            print("I am heredw")
+            assert True
 
